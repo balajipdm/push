f0b8e37ca41bf900324a559abce70ba2d9013ffd
balaji.pdm <balaji.pdm@gmail.com>
2012-07-24 23:15:47 +0530

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(:version => 20120719034006) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
-  
+
   add_index "pages", ["title"], :name => "index_pages_on_title", :unique => true
 
 end
